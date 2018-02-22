d0d86fd4a25c92d923f4f61afc4f432f72ea0f3c
Admin <janejira.ma59@snru.ac.th>
2018-02-22 16:39:00 +0700

    finish 22Fed

--- app/src/main/java/aomjanejira/snru/ac/th/emercall/fragment/HospitalFragment.java
+++ app/src/main/java/aomjanejira/snru/ac/th/emercall/fragment/HospitalFragment.java
@@ -7,8 +7,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import aomjanejira.snru.ac.th.emercall.R;
+import aomjanejira.snru.ac.th.emercall.utility.HospitelAdapter;
 
 /**
  * Created by Admin on 22/2/2561.
@@ -19,7 +21,24 @@ public class HospitalFragment extends Fragment{
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-    }
+
+//        Create ListView
+        ListView listView = getView().findViewById(R.id.listViewHopotal);
+
+        int[] ints = new int[]{R.drawable.station1, R.drawable.station2,
+                R.drawable.station3, R.drawable.station4, R.drawable.station1,
+                R.drawable.station2, R.drawable.station3, R.drawable.station4};
+
+        String[] titleStrings = new String[]{"Hospital 1 ","Hospital 2 ","Hospital 3 ",
+                "Hospital 4 ","Hospital 5 ","Hospital 6 ","Hospital 7 ","Hospital 8 ",};
+
+        String[] phoneStrings = new String[]{"1111", "1112", "1113", "1114", "1115", "1116", "1117", "1118", };
+
+        HospitelAdapter hospitelAdapter = new HospitelAdapter(getActivity(), ints, titleStrings , phoneStrings);
+        listView.setAdapter(hospitelAdapter);
+
+
+    }   // Main Method
 
     @Nullable
     @Override
